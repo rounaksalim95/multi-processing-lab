8643e474cd0d8b7f4ba6fa7ff8d6a1d64f56d8f5
rounaksalim95 <rounaksalim@gmail.com>
2022-04-26 20:29:10 -0700

    Add script to fetch papers

---
+++ paper_fetcher.py
@@ -0,0 +1,43 @@
+"""
+This script downloads Master's project papers from SJSU ScholarWorks
+for the Computer Engineering and Computer Science department
+"""
+
+import os
+import requests
+
+BASE_URL = "https://scholarworks.sjsu.edu/cgi/viewcontent.cgi?article=%s&context=etd_projects"
+
+START_INDEX = 1000
+END_INDEX = 2060
+
+
+def download_pdf(response, index):
+    """
+    Downloads the PDF obtained from the requests response object
+    response: Requests response object
+    index: (int) Index of the paper
+    """
+    with open("papers/%s.pdf" % index, "wb") as f:
+        f.write(response.content)
+
+
+def main():
+    print("Paper fetcher is starting...\n")
+
+    # Create papers directory if it doesn't exist
+    if not os.path.exists(os.getcwd() + "/papers"):
+        print("Creating papers directory")
+        os.mkdir("papers")
+
+    for i in range(START_INDEX, END_INDEX + 1):
+        r = requests.get(BASE_URL % i)
+        if r.status_code == 200:
+            print("Downloading paper at index %d" % i)
+            download_pdf(r, i)
+
+    print("\nPaper fetcher is done!")
+
+
+if __name__ == "__main__":
+    main()
