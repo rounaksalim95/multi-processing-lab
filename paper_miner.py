9ca35a99eb329828047595c0af16a8f4406bc7ab
rounaksalim95 <rounaksalim@gmail.com>
2022-05-03 21:57:57 -0700

    Parallize paper_miner.py

--- paper_miner.py
+++ paper_miner.py
@@ -9,13 +9,13 @@ import time
 from absl import app
 from absl import flags
 from absl import logging
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 from pdfminer.high_level import extract_text
 
 START_INDEX = 1000
 END_INDEX = 2060
 THREADS = 17
 PROCESSES = 8
-CHUNKSIZE = (END_INDEX - START_INDEX) // PROCESSES
 KEYWORD = "machine"
 
 FLAGS = flags.FLAGS
@@ -41,7 +41,6 @@ def func_timer(func):
     return wrapper
 
 
-@func_timer
 def search_pdf(file_path):
     """
     Searches the PDF {index}.pdf for the specified word
@@ -57,7 +56,7 @@ def search_pdf(file_path):
         for word in text.split(" "):
             if word.lower() == KEYWORD:
                 counter += 1
-        print(f"Counter is {counter} for file {file_path}")
+        logging.info(f"Counter is {counter} for file {file_path}")
         return counter
     except Exception as e:
         logging.error(f"Exception {e} while extracting text from {file_path}")
@@ -83,6 +82,32 @@ def mine_papers_sequentially(files):
     return result
 
 
+@func_timer
+def mine_papers_in_parallel(files):
+    """
+    Mine the papers in parallel
+    Args:
+        files: (list) List of files to mine
+    Returns:
+        (map) Map of file path to number of times the keyword was found in the paper
+    """
+    result = {}
+    multi_executor = None
+    if FLAGS.multiprocessing:
+        multi_executor = ProcessPoolExecutor(FLAGS.processes)
+        logging.info("Searching papers using multiprocessing")
+    else:
+        multi_executor = ThreadPoolExecutor(FLAGS.threads)
+        logging.info("Searching papers using multithreading")
+
+    with multi_executor as executor:
+        for arg, res in zip(files, executor.map(search_pdf, files, chunksize=len(files) // FLAGS.processes)):
+            if res is not None:
+                result[arg] = res
+
+    return result
+
+
 def main(argv):
     global KEYWORD
     KEYWORD = FLAGS.keyword
@@ -94,8 +119,7 @@ def main(argv):
     result = {}
 
     if FLAGS.parallel:
-        # mine_papers_in_parallel()
-        pass
+        result = mine_papers_in_parallel(files)
     else:
         result = mine_papers_sequentially(files)
 
